9397083985bd8f350b969fcb6088a335f4a95195
Christina Takara <christina.takara@gmail.com>
2022-02-02 15:04:24 -0600

    updated cats to pull cat photos from API but need to update to request by button push

--- src/pages/Cats/index.js
+++ src/pages/Cats/index.js
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
 import HighlightStory from "../../components/HighlightStory";
 import Footer from '../../components/Footer'
 
 const Cats = () => {
+
+    const [availableCats, setAvailableCats] = useState([])
+    // const getCatBreedList = async () => {
+    //     const newCatBreedList = []
+    //     try {
+    //         const response = await axios.get('https://api.thecatapi.com/v1/breeds')
+    //         // console.log(response.data.length)
+    //         response.data.forEach(breed => {
+    //             newCatBreedList.push(breed.name)
+    //             // console.log(breed.name)
+
+    //         })
+    //     }
+    //     catch (error) {
+    //         console.log(error);
+    //     }
+    //     console.log('newcatBreedList', newCatBreedList)
+    //     setAvailableCats(newCatBreedList)
+    // }
+
+    // const getCatList = async () => {
+    //     const newCatList = ['nothing', 'yet']
+    //     try {
+    //         const response = await axios.get('https://api.thecatapi.com/v1/images/search?breed_ids=beng')
+    //         console.log(response.data[0].url)
+    //     }
+    //     catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    // const setSampleCatList = (catBreedList) => {
+    //     let numCats = (Math.ceil(Math.random()*15)+5)
+    //     const catList = [];
+    //     // let numCats = 15
+    //     let maxCat = catBreedList.length;
+    //     console.log('end of breed array is', maxCat)
+
+    //     console.log('in setSampleCatList and numCats is ', numCats);
+
+    //     console.log('in setsamplecatlist and cat breedlist is ', catBreedList)
+        
+    //     for(let i=0; i<numCats; i++) {
+    //         let breedIndex = Math.floor(Math.random() * maxCat)
+    //         catList.push(catBreedList[breedIndex])
+    //     }
+    //     console.log('in setsamplecatlist and catlist is', catList)
+    //     return catList
+    // }
+
+
+    // const catBreedList = getCatBreedList();
+    // console.log('after calling get cat breed list', catBreedList);
+    // // const sampleCatList = setSampleCatList(catBreedList);
+
+
     return (
         <div>
             <h1>CATS PAGE</h1>
