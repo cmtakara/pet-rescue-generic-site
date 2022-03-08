10598c33e450bee5cfa1b2836e0372f2335c6503
Christina Takara <christina.takara@gmail.com>
2022-03-08 16:55:51 -0600

    added all of the model fields to input form

--- src/components/UpdatePeopleForm/index.js
+++ src/components/UpdatePeopleForm/index.js
@@ -13,10 +13,16 @@ const UpdatePeopleForm = ({ fetchPeople, addPerson, setEditForm, editForm, updat
     const [admin, setAdmin] = useState('N')
     const [updaterights, setUpdaterights] = useState('N')
     const [fosterstatus, setFosterstatus] = useState('NONE')
+    const [fosteravailability, setFosteravailability] = useState('')
+    const [fostercurrentanimal, setFostercurrentanimal] = useState('')
     const [volunteerstatus, setVolunteerstatus] = useState('NONE')
-    const [adopterstatus, setAdopterstus] = useState('NONE')
+    const [volunteercleaning, setVolunteercleaning] = useState('')
+    const [volunteerother, setVolunteerother] = useState('')
     const [volunteertransport, setVolunteertransport] = useState('N')
     const [volunteerevents, setVolunteerevents] = useState('N')
+    const [adopterstatus, setAdopterstus] = useState('NONE')
+    const [adopteranimaltype, setAdopteranimaltype] = useState('')
+    const [adopteranimalname, setAdopteranimalname] = useState('')
     const [notes, setNotes] = useState('')
 
     // const [updatePerson, setUpdatePerson] = useState({})
@@ -43,10 +49,16 @@ const UpdatePeopleForm = ({ fetchPeople, addPerson, setEditForm, editForm, updat
             admin: admin,
             updaterights: updaterights,
             fosterstatus: fosterstatus,
+            fosteravailability: fosteravailability,
+            fostercurrentanimal: fostercurrentanimal,
             volunteerstatus: volunteerstatus,
-            adopterstatus: adopterstatus,
+            volunteercleaning: volunteercleaning,
             volunteertransport: volunteertransport,
+            volunteerother: volunteerother,
             volunteerevents: volunteerevents,
+            adopterstatus: adopterstatus,
+            adopteranimaltype: adopteranimaltype,
+            adopteranimalname: adopteranimalname,
             notes: notes
         }
     
@@ -143,6 +155,108 @@ const UpdatePeopleForm = ({ fetchPeople, addPerson, setEditForm, editForm, updat
                 />
             </div>
             <div className="field">
+                <label className='ui left aligned container'>Foster Status</label>
+                <input
+                    type="text"
+                    name="fosterstatus"
+                    placeholder = {editForm ? updatePerson.fosterstatus : "NONE/APPLIED/APPROVED/DENIED/ACTIVE/INACTIVE"}
+                    onChange={e => setFosterstatus(e.target.value)}
+                />
+            </div>            
+            <div className="field">
+                <label className='ui left aligned container'>Foster Availability</label>
+                <input
+                    type="text"
+                    name="fosteravailability"
+                    placeholder = {editForm ? updatePerson.fosteravailability : "NONE/LIMITED/OPEN"}
+                    onChange={e => setFosteravailability(e.target.value)}
+                />
+            </div>            
+            <div className="field">
+                <label className='ui left aligned container'>Foster Current Animal</label>
+                <input
+                    type="text"
+                    name="fostercurrentanimal"
+                    placeholder = {editForm ? updatePerson.fostercurrentanimal : "Name of Animal or leave empty"}
+                    onChange={e => setFostercurrentanimal(e.target.value)}
+                />
+            </div>
+            <div className="field">
+                <label className='ui left aligned container'>Volunteer Status</label>
+                <input
+                    type="text"
+                    name="volunteerstatus"
+                    placeholder = {editForm ? updatePerson.volunteerstatus : "NONE/APPLIED/APPROVED/DENIED/ACTIVE/INACTIVE"}
+                    onChange={e => setVolunteerstatus(e.target.value)}
+                />
+            </div>            
+            <div className="field">
+                <label className='ui left aligned container'>Volunteer Cleaning Information</label>
+                <input
+                    type="text"
+                    name="volunteercleaning"
+                    placeholder = {editForm ? updatePerson.volunteercleaning : "day(s) AM/PM location"}
+                    onChange={e => setVolunteercleaning(e.target.value)}
+                />
+            </div>            
+            <div className="field">
+                <label className='ui left aligned container'>Volunteer for Transport</label>
+                <input
+                    type="text"
+                    name="volunteertransport"
+                    placeholder = {editForm ? updatePerson.volunteertransport : "Y/N"}
+                    onChange={e => setVolunteertransport(e.target.value)}
+                />
+            </div>            
+            <div className="field">
+                <label className='ui left aligned container'>Volunteer for Events</label>
+                <input
+                    type="text"
+                    name="volunteerevents"
+                    placeholder = {editForm ? updatePerson.volunteerevents : "Y/N"}
+                    onChange={e => setVolunteerevents(e.target.value)}
+                />
+            </div>                
+            <div className="field">
+                <label className='ui left aligned container'>Other Ways Willing to Volunteer</label>
+                <input
+                    type="text"
+                    name="volunteerother"
+                    placeholder = {editForm ? updatePerson.volunteerother : "specific volunteer activities, availability, and limitations"}
+                    onChange={e => setVolunteerother(e.target.value)}
+                />
+            </div>            
+
+
+
+            <div className="field">
+                <label className='ui left aligned container'>Adopter Status</label>
+                <input
+                    type="text"
+                    name="adopterstatus"
+                    placeholder = {editForm ? updatePerson.adopterstatus : "NONE/APPLIED/APPROVED/DENIED/COMPLETED"}
+                    onChange={e => setAdopterstus(e.target.value)}
+                />
+            </div>            
+            <div className="field">
+                <label className='ui left aligned container'>Adopter Animal Type</label>
+                <input
+                    type="text"
+                    name="adopteranimaltype"
+                    placeholder = {editForm ? updatePerson.adopteranimaltype : "cat/dog"}
+                    onChange={e => setAdopteranimaltype(e.target.value)}
+                />
+            </div>            
+            <div className="field">
+                <label className='ui left aligned container'>Adopter Animal Name</label>
+                <input
+                    type="text"
+                    name="adopteranimalname"
+                    placeholder = {editForm ? updatePerson.adopteranimalname : "Name of Animal or leave empty"}
+                    onChange={e => setAdopteranimalname(e.target.value)}
+                />
+            </div>
+            <div className="field">
                 <label className='ui left aligned container'>Notes</label>
                 <input
                     type="text"
@@ -158,158 +272,8 @@ const UpdatePeopleForm = ({ fetchPeople, addPerson, setEditForm, editForm, updat
                 </button>
             </div>
         </form>
+
     
-
-      
-            {/* <form onSubmit={handleSubmit}>
-                <div className="m-3 flex-cont">
-                    <label htmlFor="animalName" class="form-label">Animal's Name</label>
-                    <input type="text" class="form-control" name='animalName' />
-                </div>
-                <div className="m-3 flex-cont justify-content-around flex-wrap">
-                    <label htmlFor="animalType" class="form-label">
-                        Choose cat or dog
-                        <select value={petType} onChange={handleTypeSelect}>
-                            <option value="">  </option>
-                            <option value="cat">cat</option>
-                            <option value="dog">dog</option>
-                        </select>
-                    </label>
-                    <label htmlFor="vetting" class="form-label">
-                        Choose Y or N for vetting completed  (choose N if unknown)
-                        <select value={vetting} onChange={handleVettingSelect}>
-                            <option value="Y">Y</option>
-                            <option value="N">N</option>
-                        </select>
-                    </label>
-                    <label htmlFor="bonded" class="form-label">
-                        Choose Y or N for bonded 
-                        <select value={bonded} onChange={handleBondedSelect}>
-                            <option value="Y">Y</option>
-                            <option value="N">N</option>
-                        </select>
-                    </label>
-                    <label htmlFor="chip" class="form-label">
-                        Choose Y or N for microchip (choose N if unknown)
-                        <select value={chip} onChange={handleChipSelect}>
-                            <option value="Y">Y</option>
-                            <option value="N">N</option>
-                        </select>
-                    </label>
-                    <label htmlFor="spayneuter" class="form-label">
-                        Choose unknown, Y, or N for spay/neuter status
-                        <select value={spayneuter} onChange={handleNeuterSelect}>
-                            <option value="unknown">unknown</option>
-                            <option value="Y">Y</option>
-                            <option value="N">N</option>
-                        </select>
-                    </label>
-                    <div class="flex-cont justify-content-around" >
-                        <label htmlFor="adoptionstatus" class="form-label">
-                            Choose adoption status
-                            <select value={adoptionstatus} onChange={handleAdoptionStatusSelect}>
-                                <option value="NONE">NONE</option>
-                                <option value="APPLIED">APPLIED</option>
-                                <option value="APPROVED">APPROVED</option>
-                                <option value="COMPLETED">COMPLETED</option>
-                            </select>
-                            <label htmlFor="adopter" class="form-label">Adopter's Name</label>
-                            <input type="text" class="form-control" name='adopter' />
-                        </label>
-                    </div>
-                </div>
-                <div className="m-3 flex-cont justify-content-around">
-                    <label htmlFor="ageNum" class="form-label">Animal's Age</label>
-                    <input type="number" class="form-control" name='ageNum' />
-                    <label htmlFor="ageUnit" class="form-label">
-                        Choose the units for age (weeks/months/years)
-                        <select value={ageUnit} onChange={handleAgeSelect}>
-                            <option value="">   </option>
-                            <option value="weeks">weeks</option>
-                            <option value="months">months</option>
-                            <option value="years">years</option>
-                        </select>
-                    </label>
-
-                </div>
-                <div className="mb-3 flex-cont">
-                    <label htmlFor="imagename" class="form-label">Image name (Short Caption)</label>
-                    <input type="text" class="form-control" name='imagename' />
-
-                    <label htmlFor="imageurl" class="form-label">Image URL (link to picture)</label>
-                    <input type="text" class="form-control" name='imageurl' />
-                </div>
-                <div className="mb-3 flex-cont">
-                    <label htmlFor="foster" class="form-label">Foster's name (or NONE)</label>
-                    <input type="text" class="form-control" name='foster' />
-
-                    <label htmlFor="location" class="form-label">Animal's Location</label>
-                    <input type="text" class="form-control" name='location' />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="issues" class="form-label">Known Issues</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name='issues'></textarea>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="other" class="form-label">Notes</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name='other'></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Save</button>
-
-            </form>
-
-            <div>
-                This is the current animal, it is a {petType} <br></br>
-                {newDisplay} <br></br>
-                Do you want to add this {petType}?
-                <form onSubmit={handleAdd}>
-                    <button type="submit" class="btn btn-primary">Add</button>
-                </form>
-            </div>
-
-            <div>
-
-
-            <form onSubmit={displayAnimals}>
-                <label htmlFor="animalType" class="form-label">
-                        Choose cat or dog
-                        <select value={petType} onChange={handleTypeSelect}>
-                            <option value="cat">cat</option>
-                            <option value="dog">dog</option>
-                        </select>
-                    </label>
-                    <button type="submit" class="btn btn-primary">Display existing</button>
-            </form> */}
-
-{/* 
-            <table className="ui celled table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Vetting</th>
-                        <th>image name</th>
-                        <th>image url</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        availableCats.map(animal => {
-                            return (
-                                <tr key={animal.id}>
-                                    <td data-label="Name">{animal.name}</td>
-                                    <td data-label="Age">{animal.age}</td>
-                                    <td data-label="Vetting">{animal.vetting}</td>
-                                    <td data-label="Image Name">{animal.imagename}</td>
-                                    <td data-label="Image URL">{animal.imageurl}</td>
-                                </tr>
-                            )
-                        })
-                    }
-
-                </tbody>
-            </table> */}
-            {/* </div> */}
         </div>
     );
 }
